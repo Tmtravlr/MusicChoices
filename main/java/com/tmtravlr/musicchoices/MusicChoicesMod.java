47404f662bc18849cdc327f2fcb5b3dec3fafe69
Tmtravlr <tmtravlrsmail@gmail.com>
2015-04-13 17:39:27 -0600

    Set things up for 1.8

--- main/java/com/tmtravlr/musicchoices/MusicChoicesMod.java
+++ main/java/com/tmtravlr/musicchoices/MusicChoicesMod.java
@@ -35,7 +35,7 @@ public class MusicChoicesMod
 {
 	
     public static final String MODID = "musicchoices";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0_for_1.7.10";
     
     @Instance(MODID)
 	public static MusicChoicesMod musicChoices;
